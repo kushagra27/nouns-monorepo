5eab2220d3c43b9b9b5d0e04cb36ef658d400e90
oovg <office@vengist.com>
2022-07-15 17:27:28 +0200

    chainId.rinkeby on avatarprovider

--- packages/nouns-webapp/src/App.tsx
+++ packages/nouns-webapp/src/App.tsx
@@ -47,7 +47,7 @@ function App() {
       )}
       <BrowserRouter>
         <AvatarProvider
-          provider={chainId === ChainId.Mainnet ? library : undefined}
+          provider={chainId === ChainId.Rinkeby ? library : undefined}
           batchLookups={true}
         >
           <NavBar />
